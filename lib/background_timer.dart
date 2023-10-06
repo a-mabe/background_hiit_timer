862634c9445d8c0f059f3f023d98dc57e929ed92
a-mabe <mabe.abby.a@gmail.com>
2023-10-06 10:14:05 -0400

    Show circular progres indicator when the timer is complete

--- lib/background_timer.dart
+++ lib/background_timer.dart
@@ -239,7 +239,7 @@ class CountdownState extends State<Countdown> with WidgetsBindingObserver {
             _numberOfIntervals,
             _paused);
 
-        if (_currentMicroSeconds <= 0) {
+        if (_currentMicroSeconds <= 0 && _status == "complete") {
           return const Center(
             child: CircularProgressIndicator(),
           );
