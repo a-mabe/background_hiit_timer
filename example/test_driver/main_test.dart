c233ac382d4eb5b08057b4579f294cc1ea6fad2e
a-mabe <mabe.abby.a@gmail.com>
2024-11-08 20:47:20 -0500

    print statements in test

--- example/test_driver/main_test.dart
+++ example/test_driver/main_test.dart
@@ -16,55 +16,31 @@ void main() {
 
   test('Timer integration test with timeout and debug', () async {
     // Wait for "Get ready" text
+    print('Waiting for "Get ready" text');
     await driver!
         .waitFor(find.text('Get ready'), timeout: const Duration(seconds: 10));
 
     // Proceed to next state with delay
     await Future.delayed(const Duration(seconds: 11));
+    print('Waiting for "Warmup" text');
     await driver!
         .waitFor(find.text('Warmup'), timeout: const Duration(seconds: 5));
 
     // Check for next state
     await Future.delayed(const Duration(seconds: 11));
+    print('Waiting for "Work" text');
     await driver!
         .waitFor(find.text('Work'), timeout: const Duration(seconds: 5));
 
     // Find and tap the restart icon.
+    print('Tapping restart icon');
     final restartIcon = find.byTooltip('Restart');
     await driver!.tap(restartIcon);
 
     // Wait for "Get ready" text
     await Future.delayed(const Duration(seconds: 5));
+    print('Waiting for "Get ready" text');
     await driver!
         .waitFor(find.text('Get ready'), timeout: const Duration(seconds: 10));
   });
-
-  // test('Timer integration test', () async {
-  //   // Wait until the text "Get ready" is found.
-  //   await driver!.waitFor(find.text('Get ready'));
-
-  //   // Wait for 11 seconds, assuming the timer updates automatically.
-  //   await Future.delayed(const Duration(seconds: 11));
-
-  //   // Check that the text has updated to "Warmup".
-  //   expect(await driver!.getText(find.text('Warmup')), 'Warmup');
-
-  //   // Wait for an additional 20 seconds.
-  //   await Future.delayed(const Duration(seconds: 20));
-
-  //   // Check that the text has updated to "Work".
-  //   expect(await driver!.getText(find.text('Work')), 'Work');
-
-  //   // Find and tap the restart icon.
-  //   final restartIcon = find.byTooltip(
-  //       'Restart'); // Ensure the icon has a tooltip for easier finding
-  //   await driver!.tap(restartIcon);
-
-  //   // Wait at most 10 seconds for "Get ready" to be back on screen.
-  //   await driver!
-  //       .waitFor(find.text('Get ready'), timeout: const Duration(seconds: 10));
-
-  //   // Find and tap the "Stop" button.
-  //   await driver!.tap(find.text('Stop'));
-  // });
 }
