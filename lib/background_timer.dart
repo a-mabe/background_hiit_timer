4a22c6672366366eca2fa797f9e297bf652a45f0
a-mabe <mabe.abby.a@gmail.com>
2025-03-22 16:28:29 -0400

    test3

--- lib/background_timer.dart
+++ lib/background_timer.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 import 'dart:io';
 import 'dart:ui';
-import 'package:openhiit_audioplayers/openhiit_audioplayers.dart';
+import 'package:audioplayers/audioplayers.dart';
 import 'package:background_hiit_timer/background_timer_controller.dart';
 import 'package:background_hiit_timer/models/interval_type.dart';
 import 'package:background_hiit_timer/utils/database.dart';
@@ -226,6 +226,10 @@ class CountdownState extends State<Countdown> with WidgetsBindingObserver {
     SharedPreferences preferences,
     TimerState timerState,
   ) async {
+    await AudioPlayer.global.setAudioContext(
+        AudioContextConfig(focus: AudioContextConfigFocus.mixWithOthers)
+            .build());
+
     if (service is AndroidServiceInstance) {
       service.on('setAsForeground').listen((event) {
         service.setAsForegroundService();
